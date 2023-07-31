4a4a1efea69477ddebb5847a497f2f824c3fae15
Domejko <sllawny@gmail.com>
2023-07-31 19:08:31 +0200

    initial commit

--- app/models.py
+++ app/models.py
@@ -7,35 +7,35 @@ from .database import Base
 
 class User(Base):
     __tablename__ = 'users'
-    id = Column(Integer, nullable=False)
+    id = Column(Integer, nullable=False, primary_key=True)
     email = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
     
-    PrimaryKeyConstraint(id)
+    # PrimaryKeyConstraint(id)
 
 
 class Post(Base):
     __tablename__ = 'posts'
-    id = Column(Integer, nullable=False)
+    id = Column(Integer, nullable=False, primary_key=True)
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
     published = Column(Boolean, server_default='TRUE', nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
     updated_at = Column(TIMESTAMP, onupdate=text('now()'), nullable=True)
-    user_id = Column(Integer, nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
 
-    ForeignKeyConstraint(['user_id'], ['users.id'], ondelete='CASCADE')
-    PrimaryKeyConstraint(id)
+    # ForeignKeyConstraint(['user_id'], ['user.id'], ondelete='CASCADE')
+    # PrimaryKeyConstraint(id)
 
     owner = relationship('User')
     
 
 class Vote(Base):
     __tablename__ = 'votes'
-    post_id = Column(Integer, nullable=False)
-    user_id = Column(Integer, nullable=False)
+    post_id = Column(Integer, ForeignKey('posts.id', ondelete='CASCADE'), primary_key=True, nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id', ondelete='CASCADE'), primary_key=True, nullable=False)
 
-    ForeignKeyConstraint(['post_id'], ['posts.id'], ondelete='CASCADE')
-    ForeignKeyConstraint(['user_id'], ['users.id'], ondelete='CASCADE')
-    PrimaryKeyConstraint(post_id, user_id)
+    # ForeignKeyConstraint(['post_id'], ['posts.id'], ondelete='CASCADE')
+    # ForeignKeyConstraint(['user_id'], ['user.id'], ondelete='CASCADE')
+    # PrimaryKeyConstraint(post_id, user_id)
